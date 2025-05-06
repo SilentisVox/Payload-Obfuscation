f889225befc10c79e2415b8db96f6aca8de5ac5a
Silent <165338136+SilentisVox@users.noreply.github.com>
2025-05-05 23:23:32 -0400

    Update GenerateMac.h

--- MAC Obfuscation/Obfuscate Payload/GenerateMac.h
+++ MAC Obfuscation/Obfuscate Payload/GenerateMac.h
@@ -1,20 +1,10 @@
-#pragma once
+#include <windows.h>
+#include <stdio.h>
 
 #ifndef GENERATEMAC_H
 #define GENERATEMAC_H
 
-#include <windows.h>
-#include <stdio.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-	VOID GenerateMAC(IN INT A, IN INT B, IN INT C, IN INT D, IN INT E, IN INT F, OUT PCHAR mac_string);
-	BOOL GenerateMacOutput(IN PBYTE shellcode, IN SIZE_T shellcode_size);
-
-#ifdef __cplusplus
-}
-#endif
+VOID GenerateMAC(IN INT A, IN INT B, IN INT C, IN INT D, IN INT E, IN INT F, OUT PCHAR mac_string);
+BOOL GenerateMacOutput(IN PBYTE shellcode, IN SIZE_T shellcode_size);
 
 #endif // GENERATEMAC_H
